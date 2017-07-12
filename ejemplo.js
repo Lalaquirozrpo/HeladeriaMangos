c90f53f2713f324caee392d9beb3f4eb9e587553
Laura Quiroz <quinterocami@hotmail.com>
2017-07-12 09:52:51 -0500

    ejemplo de javascript con alert y confirm

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+
+if (confirm("Estas contento enclase?")){
+	//Lo que se hace si se cumple la funcion 
+	alert("me caes bien");
+}else{
+	//lo que se hace si no se cumple la funcion 
+	alert("mejor quedese en la casa");
+}
+
